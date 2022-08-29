3ad600cdfd8a636fdedad3de51db9a3c2d8ee2ba
igor-s-ribeiro <igorsampaio10@outlook.com>
2022-08-29 17:40:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Elabore um algoritmo que calcule a idade média de 5 alunos.
+
+double media = 0;
+double idade1 = 0;
+double idade2 = 0;
+double idade3 = 0;
+double idade4 = 0;
+double idade5 = 0;
+
+
+Console.WriteLine("Programa que calcula a idade media de cinco alunos");
+Console.Write("Informe a idade do 1° aluno: ");
+idade1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a idade do 2° aluno: ");
+idade2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a idade do 3° aluno: ");
+idade3 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a idade do 4° aluno: ");
+idade4 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a idade do 5° aluno: ");
+idade5 = Convert.ToDouble(Console.ReadLine());
+
+media=(idade1+idade2+idade3+idade4+idade5) / 5;
+Console.WriteLine("A média entre as idades é: " +media);
+
+ Console.ReadKey();
